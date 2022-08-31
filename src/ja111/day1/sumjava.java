a01c94fdc217ed9086dd98599465493e43c9d095
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2022-08-31 22:01:10 +0530

    Masai JA111 Day1

---
+++ src/ja111/day1/sumjava.java
@@ -0,0 +1,17 @@
+package ja111.day1;
+
+public class sumjava {
+    public static void main(String[] args){
+        System.out.println("Total Sum");
+        sum();
+    }
+
+    static void sum(){
+        int a;
+        int b;
+        a = 50;
+        b = 7;
+        int c = a+b;
+        System.out.println(c);
+    }
+}

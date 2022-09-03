e833f2b0fea65aa21df8bd2d0cb6f5fc012d3ffa
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2022-09-03 15:40:00 +0530

    statements, itrative, return and continue

---
+++ src/ja111/day3/_1ObjectDemo.java
@@ -0,0 +1,18 @@
+package ja111.day3;
+
+public class _1ObjectDemo {
+    public static void main(String[] main){
+        //create object of the class A
+        //let a = new A();
+        //syntax
+        //Type var_name = new class()
+        A a = new A();
+    }
+}
+
+class A{
+    A(){
+        System.out.println("Constructor called");
+    }
+}
+

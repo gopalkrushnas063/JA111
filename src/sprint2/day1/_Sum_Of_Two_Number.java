4d4e31a0bd6e93bdb81c3ea91cc7379337799c3c
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-09-06 13:54:36 +0530

    Object create using java

---
+++ src/sprint2/day1/_Sum_Of_Two_Number.java
@@ -0,0 +1,9 @@
+package sprint2.day1;
+
+public class _Sum_Of_Two_Number {
+    public static void main(String[] args){
+        int a = 9;
+        int b = 5;
+        System.out.println(a+b);
+    }
+}

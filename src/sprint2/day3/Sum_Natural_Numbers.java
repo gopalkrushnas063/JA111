e5cfddd60934caae56d816d2c7a8fcb60cc82cff
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-09-06 21:54:10 +0530

    SUM OF NATURAL NUMBERS

---
+++ src/sprint2/day3/Sum_Natural_Numbers.java
@@ -0,0 +1,10 @@
+package sprint2.day3;
+
+public class Sum_Natural_Numbers {
+    public static void main(String[] args) {
+        int n = 15;
+        int natural_num = ((n*(n+1))/2);
+        System.out.println(natural_num);
+
+    }
+}

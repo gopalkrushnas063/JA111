660a55739a87f343719f89f18c778d7777ed1291
GOPAL KRUSHNA SAHOO <gopalkrushnas063@gmail.com>
2022-09-06 13:54:36 +0530

    Object create using java

---
+++ src/sprint2/day1/MyObj.java
@@ -0,0 +1,13 @@
+package sprint2.day1;
+
+public class MyObj {
+    int x = 5;
+
+    public static void main(String[] args) {
+        MyObj myObj1 = new MyObj();
+        MyObj myObj2 = new MyObj();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+
+    }
+}
---
+++ src/sprint2/day1/Object_Class.java
@@ -0,0 +1,12 @@
+package sprint2.day1;
+
+public class Object_Class {
+    public static void main(String[] args){
+        Object_Class obj = new Object_Class();
+        obj.show();
+    }
+
+    void show(){
+        System.out.println("First Object Create Using Java");
+    }
+}
